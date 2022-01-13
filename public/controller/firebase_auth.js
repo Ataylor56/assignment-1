f336338cc0045e05324b11ccd1a4e954a7d10563
Austin Taylor <Austin@AMB0003.local>
2022-01-12 21:36:41 -0600

    pre error

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,10 +1,11 @@
-import { getAuth, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js'
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js'
 import * as Elements from '../viewpage/elements.js'
 import * as Util from '../viewpage/util.js'
+import * as Constants from '../model/constants.js'
 
 const auth = getAuth();
 
-
+export let currentUser = null;
 
 export function addEventListeners() {
     Elements.formSignIn.addEventListener('submit', async e => {
@@ -14,13 +15,15 @@ export function addEventListeners() {
 
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            console.log(`sign in success: ${user}`)
+            Elements.modalSignin.hide();
+            
         } catch (error) {
             const errorCode = error.code;
             const errorMessage = error.message;
             Util.info('Sign in error', JSON.stringify(error), Elements.modalSignin)
+            if (Constants.DEV) {
             console.log(`error: ${errorCode} | ${errorMessage}`)
+            }
         }
     });
 
@@ -30,8 +33,40 @@ export function addEventListeners() {
             await signOut(auth);
             console.log('sign out success');
         } catch (error) {
+            Util.info('SIgn Out Error', JSON.stringify(error))
+            if (Constants.DEV) {
             console.log(`sign out error: ${error}`);
+            }
         }
     });
+
+    onAuthStateChanged(auth, authStateChangedObserver);
+
+}
+
+function authStateChangedObserver(user) {
+    if (user) {
+        // signed in
+        currentUser = user;
+        let elements = document.getElementsByClassName('modal-preauth');
+        for (let i=0; i< elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
+        elements = document.getElementsByClassName('modal-postauth');
+        for (let i=0; i< elements.length; i++) {
+            elements[i].style.display = 'block';
+        }
+    } else {
+        // signed out
+        currentUser = null;
+        let elements = document.getElementsByClassName('modal-preauth');
+        for (let i=0; i< elements.length; i++) {
+            elements[i].style.display = 'block';
+        }
+        elements = document.getElementsByClassName('modal-postauth');
+        for (let i=0; i< elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
+    }
 }
 
--- public/viewpage/elements.js
+++ public/viewpage/elements.js
@@ -1,5 +1,15 @@
+// main root
+export const root = document.getElementById('root');
+
+// forms
 export const formSignIn = document.getElementById("form-signin");
+
+// menu buttons
 export const menuSignOut = document.getElementById('menu-signout');
+export const menuHome = document.getElementById('menu-home');
+export const menuAbout = document.getElementById('menu-about')
+
+// modals
 export const modalSignin = new bootstrap.Modal(document.getElementById('modal-signin-form'), { backdrop: 'static' } );
 export const modalInfobox = {
     modal: new bootstrap.Modal(document.getElementById('modal-infobox'), { backdrop: 'static' } ),
